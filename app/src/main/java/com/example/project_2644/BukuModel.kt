f38a0ac210252543746eca08cff46f4a54ecb420
amikom <bayudwik25@gmail.com>
2023-10-11 15:02:26 +0700

    update

---
+++ app/src/main/java/com/example/project_2644/BukuModel.kt
@@ -0,0 +1,4 @@
+package com.example.project_2644
+
+class BukuModel (var image:Int, var title:String, var desc:String){
+}
--- app/src/main/java/com/example/project_2644/HomeActivity.kt
+++ app/src/main/java/com/example/project_2644/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,26 @@ class HomeActivity : AppCompatActivity() {
             val Intent = Intent(this,BookKidActivity::class.java)
             startActivity(Intent)
         }
+        val rvBuku:RecyclerView = findViewById(R.id.recyclerViewBuku)
+
+        rvBuku.layoutManager = LinearLayoutManager(this)
+
+        val data = ArrayList<BukuModel>()
+        data.add(BukuModel(R.drawable.book1,"Emi's Beach Adventure",
+            "Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+        data.add(BukuModel(R.drawable.book4,"Emi's Beach Adventure",
+            "Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+        data.add(BukuModel(R.drawable.book2,"Emi's Beach Adventure",
+            "Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+        data.add(BukuModel(R.drawable.book1,"Emi's Beach Adventure",
+            "Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+        data.add(BukuModel(R.drawable.book2,"Emi's Beach Adventure",
+            "Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+        data.add(BukuModel(R.drawable.book4,"Emi's Beach Adventure",
+            "Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+
+        val adapter = AdapterHome(data)
+
+        rvBuku.adapter = adapter
     }
 }
